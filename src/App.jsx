ac70f9d4c8de5187cfee08dd9c5a66c968767279
Sagor <sagor.heaking@gmail.com>
2023-08-14 06:34:16 +0600

    password validation complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,10 @@
-import { useForm } from "react-hook-form";
 
 const App = () => {
-  const { register, handleSubmit,  } = useForm();
-  const formHandle = data =>{
-    console.log(data)
-  }
-
   return (
-    <div className="hero min-h-screen bg-base-200">
-  <div className="hero-content  ">
-    <div className="text-center lg:text-left">
-      <h1 className="text-5xl font-bold">Login now!</h1>
-      <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+    <div>
+      
     </div>
-    <div className="card flex-shrink-0 w-1/2 shadow-2xl bg-base-100">
-      <form onSubmit={handleSubmit(formHandle)} className="card-body">
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Email</span>
-          </label>
-          <input type="email" {...register("email")} placeholder="email" className="input input-bordered" />
-        </div>
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Password</span>
-          </label>
-          <input type="text" {...register("passowrd")} placeholder="password" className="input input-bordered" />
-          <label className="label">
-            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-          </label>
-        </div>
-        <div className="form-control mt-6">
-          <input type='submit' className="btn btn-primary">Login</input>
-        </div>
-      </form>
-    </div>
-  </div>
-</div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
