abeb4d2723cef892d130ad5d3e2ebef855533d39
Sagor <sagor.heaking@gmail.com>
2023-08-14 07:04:39 +0600

    sum style added in page

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -11,67 +11,91 @@ const Login = () => {
     console.log(data);
   };
   return (
-    <div className="hero min-h-screen bg-base-200">
-      <div className="hero-content  ">
-        <div className="text-center lg:text-left">
-          <h1 className="text-5xl font-bold">Login now!</h1>
-          <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
-          </p>
+    <div className="bg-green-300">
+        <div className="text-center text-animation ">
+            <h1 className="font-extrabold text-5xl uppercase py-6 ">wellcome to login </h1>
+            <h3 className="text-3xl pb-4 ">please login</h3>
         </div>
-        <div className="card flex-shrink-0 w-1/2 shadow-2xl bg-base-100">
-          <form onSubmit={handleSubmit(formHandle)} className="card-body">
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Email</span>
-              </label>
-              <input
-                type="text"
-                {...register("email")}
-                placeholder="email"
-                className="input input-bordered"
-              />
-            </div>
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Password</span>
-              </label>
-              <input
-                type="text"
-                {...register("password", {
-                  required: true,
-                  minLength: 6,
-                  maxLength: 20,
-                  pattern:
-                    /(?=.*?[A-Z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-])(?=.*?[0-9])/,
-                })}
-                placeholder="password"
-                className="input input-bordered"
-              />
-              {errors.password?.type === "required" && (
-                <p className="text-red-400">password is required</p>
-              )}
-              {errors.password?.type === "minLength" && (
-                <p className="text-red-400">minimum 6 character</p>
-              )}
-              {errors.password?.type === "maxLength" && (
-                <p className="text-red-400">Maximum 20 character</p>
-              )}
-              {errors.password?.type === "pattern" && (
-                <p className="text-red-400">At least one special character, one uppercase leter, one number</p>
-              )}
-              <label className="label">
-                <a href="#" className="label-text-alt link link-hover">
-                  Forgot password?
-                </a>
-              </label>
-            </div>
-            <div className="form-control mt-6">
-              <button className="btn btn-primary">Login</button>
-            </div>
-          </form>
+      <div className="hero ">
+        <div className="hero-content  ">
+          <div className="text-center lg:text-left ">
+            <h1 className="text-5xl font-bold">Login now!</h1>
+            <p className="py-6">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+          </div>
+          <div className="card flex-shrink-0  w-1/2 shadow-2xl bg-base-100">
+            <form onSubmit={handleSubmit(formHandle)} className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">UserName</span>
+                </label>
+                <input
+                  type="text"
+                  {...register("userName")}
+                  placeholder="UserName"
+                  className="input input-bordered"
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">
+                    Email<span className="text-red-400 text-xl">*</span>
+                  </span>
+                </label>
+                <input
+                  type="email"
+                  {...register("email")}
+                  placeholder="email"
+                  className="input input-bordered"
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">
+                    Password<span className="text-red-400 text-xl">*</span>
+                  </span>
+                </label>
+                <input
+                  type="text"
+                  {...register("password", {
+                    required: true,
+                    minLength: 6,
+                    maxLength: 20,
+                    pattern:
+                      /(?=.*?[A-Z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-])(?=.*?[0-9])/,
+                  })}
+                  placeholder="password"
+                  className="input input-bordered"
+                />
+                {errors.password?.type === "required" && (
+                  <p className="text-red-400">password is required</p>
+                )}
+                {errors.password?.type === "minLength" && (
+                  <p className="text-red-400">minimum 6 character</p>
+                )}
+                {errors.password?.type === "maxLength" && (
+                  <p className="text-red-400">Maximum 20 character</p>
+                )}
+                {errors.password?.type === "pattern" && (
+                  <p className="text-red-400">
+                    At least one special character, one uppercase leter, one
+                    number
+                  </p>
+                )}
+                <label className="label">
+                  <a href="#" className="label-text-alt link link-hover">
+                    Forgot password?
+                  </a>
+                </label>
+              </div>
+              <div className="form-control mt-6">
+                <button className="btn btn-primary">Login</button>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,31 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        background: 'background 10s ease infinite',
+        borderAnimation: 'rotate 10s linear infinite',
+      },
+      keyframes: {
+        background: {
+          '0%, 100%': {
+            'background-size': '200% 200%',
+            'background-position': 'left center',
+          },
+
+          '25%, 75%': {
+            'background-size': '200% 200%',
+            'background-position': 'right center',
+          },
+        },
+        rotate:{
+         ' 100%' : {
+            'transform': 'rotate(1turn)',
+        }
+
+        }
+      },
+    },
   },
   plugins: [require("daisyui")],
 }
